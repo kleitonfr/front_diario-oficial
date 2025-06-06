44cfa84dc4ab102fc94eacd83e1bfb2a973bc82e
KLEITON SILVA FERREIRA <25606@pmc.local>
2025-06-06 15:57:41 -0300

    mudança na estrutura no header e nav html

--- js/ajuste.js
+++ js/ajuste.js
@@ -1,12 +1,12 @@
 //inicializar biblioteca:
 AOS.init({
     duration: 1000,
-    once: false 
+    once: false
 })
 
 
 //atualizar data no header:
-const atualizarData = () =>  {
+const atualizarData = () => {
     const agora = new Date();
 
     const formato = {
@@ -16,7 +16,7 @@ const atualizarData = () =>  {
         year: 'numeric'
     }
 
-    let dataFormatada = agora.toLocaleDateString('pt-BR',formato)
+    let dataFormatada = agora.toLocaleDateString('pt-BR', formato)
     dataFormatada = dataFormatada.charAt(0).toUpperCase() + dataFormatada.slice(1)
     document.getElementById('dataAtual').textContent = dataFormatada
 }
@@ -30,15 +30,36 @@ document.addEventListener('DOMContentLoaded', () => {
 const contraste = document.getElementById('contraste')
 
 contraste.addEventListener('click', function () {
-  document.body.classList.toggle('contrast')
+    document.body.classList.toggle('contrast')
 })
 
 
-//paginação:
+//dropdown no nav:
+/*
+const menu1 = document.getElementById('menu1')
+const menu2 = document.getElementById('menu2')
+const menuItens = document.querySelectorAll('.menu')
 
 
+function aplicarDropdown() {
 
+    if (window.innerWidth > 549) {
+        menu1.classList.add('dropdown')
+        menu2.classList.add('dropdown-menu')
+        menuItens.classList.add('dropdown-item')
+    }
+    else {        
+        menu1.classList.remove('dropdown')
+        menu2.classList.remove('dropdown-menu')
+        menuItens.classList.remove('dropdown-item')
+    }
 
+}
+
+window.addEventListener('laod', aplicarDropdown())
+window.addEventListener('resize', aplicarDropdown())
+
+*/
 
 
 
