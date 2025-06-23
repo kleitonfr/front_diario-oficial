83e4baad22719a55f017f39adae0f0d9864392d1
KLEITON SILVA FERREIRA <25606@pmc.local>
2025-06-23 16:25:12 -0300

    font size aplicado para quade toda page, falta cards

--- js/ajuste.js
+++ js/ajuste.js
@@ -38,51 +38,110 @@ contraste.addEventListener('click', () => {
 
     //fonte size:
 
-let tamanhoAtual = 80
-let tamanhoAtualT1 = 400
-let tamanhoAtualT2 = 500
-let tamanhoAtualT3 = 150
-let tamanhoAtualConsulta = 150
+let tamanhoPadraoPx = parseInt(getComputedStyle(document.documentElement).fontSize);
+let cardSize = document.querySelector(".fonteCard")
+let elementosCard = cardSize.querySelectorAll("p, small, h6")
+
+let tamanhoAtualNav = (parseFloat(getComputedStyle(document.querySelector(".fonteMaxMinNav")).fontSize)) / tamanhoPadraoPx * 100
+let tamanhoAtualData = (parseFloat(getComputedStyle(document.querySelector(".fonteMaxMinData")).fontSize)) / tamanhoPadraoPx * 100
+let tamanhoAtualT1 = (parseFloat(getComputedStyle(document.querySelector(".fonteMaxMin-t1")).fontSize)) / tamanhoPadraoPx * 100
+let tamanhoAtualT2 = (parseFloat(getComputedStyle(document.querySelector(".fonteMaxMin-t2")).fontSize)) / tamanhoPadraoPx * 100
+let tamanhoAtualT3 = (parseFloat(getComputedStyle(document.querySelector(".fonteMaxMin-t3")).fontSize)) / tamanhoPadraoPx * 100
+let tamanhoAtualT4 = (parseFloat(getComputedStyle(document.querySelector(".fonteMaxMin-t4")).fontSize)) / tamanhoPadraoPx * 100
+let tamanhoAtualT5 = (parseFloat(getComputedStyle(document.querySelector(".fonteMaxMin-t5")).fontSize)) / tamanhoPadraoPx * 100
+let tamanhoAtualT6 = (parseFloat(getComputedStyle(document.querySelector(".fonteMaxMin-t6")).fontSize)) / tamanhoPadraoPx * 100
+let tamanhoAtualT7 = (parseFloat(getComputedStyle(document.querySelector(".fonteMaxMin-t7")).fontSize)) / tamanhoPadraoPx * 100
+let tamanhoAtualT8 = (parseFloat(getComputedStyle(document.querySelector(".fonteMaxMin-t8")).fontSize)) / tamanhoPadraoPx * 100
+let tamanhoAtualT9 = (parseFloat(getComputedStyle(document.querySelector(".fonteMaxMin-t9")).fontSize)) / tamanhoPadraoPx * 100
+let tamanhoAtualT10 = (parseFloat(getComputedStyle(document.querySelector(".fonteMaxMin-t10")).fontSize)) / tamanhoPadraoPx * 100
+let tamanhoAtualCard = elementosCard.forEach((el) => { parseFloat(getComputedStyle(el).fontSize)/tamanhoPadraoPx *100})
+
 
 const btnMaxFonte = document.getElementById("btnMaxFonte")
 const btnMinFonte = document.getElementById("btnMinFonte")
 
-const fonteMaxMin = document.querySelectorAll(".fonteMaxMin")
-const titulo1 = document.querySelector(".fonteMaxMin-t1")
-const titulo2 = document.querySelector(".fonteMaxMin-t2")
-const titulo3 = document.querySelector(".fonteMaxMin-t3")
-const consulta = document.querySelectorAll(".fonteMaxMin-consulta")
+const fonteMaxMinNav = document.querySelector(".fonteMaxMinNav")
+const fonteMaxMinData = document.querySelector(".fonteMaxMinData")
+const fonteMaxMinT1 = document.querySelector(".fonteMaxMin-t1")
+const fonteMaxMinT2 = document.querySelector(".fonteMaxMin-t2")
+const fonteMaxMinT3 = document.querySelector(".fonteMaxMin-t3")
+const fonteMaxMinT4 = document.querySelector(".fonteMaxMin-t4")
+const fonteMaxMinT5 = document.querySelector(".fonteMaxMin-t5")
+const fonteMaxMinT6 = document.querySelector(".fonteMaxMin-t6")
+const fonteMaxMinT7 = document.querySelector(".fonteMaxMin-t7")
+const fonteMaxMinT8 = document.querySelector(".fonteMaxMin-t8")
+const fonteMaxMinT9 = document.querySelector(".fonteMaxMin-t9")
+const fonteMaxMinT10 = document.querySelector(".fonteMaxMin-t10")
+const fonteCard = elementosCard
+
+
 
 
 btnMaxFonte.addEventListener('click', () =>{
-    if(tamanhoAtual < 200 || tamanhoAtualT1 < 800 || tamanhoAtualT2 < 800 || tamanhoAtualT3 < 500) {
-        tamanhoAtual += 5
-        tamanhoAtualT1 += 40
-        tamanhoAtualT2 += 40
-        tamanhoAtualT3 += 20
+    if(tamanhoAtualNav <= 150) tamanhoAtualNav += 10
+    if(tamanhoAtualData <= 150) tamanhoAtualData += 10
+    if(tamanhoAtualT1 <= 800) tamanhoAtualT1 += 30
+    if(tamanhoAtualT2 <= 800) tamanhoAtualT2 += 30
+    if(tamanhoAtualT3 <= 400) tamanhoAtualT3 += 20
+    if(tamanhoAtualT4 <= 150) tamanhoAtualT4 += 10
+    if(tamanhoAtualT5 <= 150) tamanhoAtualT5 += 10
+    if(tamanhoAtualT6 <= 600) tamanhoAtualT6 += 10
+    if(tamanhoAtualT7 <= 150) tamanhoAtualT7 += 10
+    if(tamanhoAtualT8 <= 150) tamanhoAtualT8 += 10
+    if(tamanhoAtualT9 <= 150) tamanhoAtualT9 += 10
+    if(tamanhoAtualT10 <= 600) tamanhoAtualT10 += 20
+    if(tamanhoAtualCard <= 150) tamanhoAtualCard += 10
+  
+
+    fonteMaxMinNav.style.fontSize = tamanhoAtualNav + "%"
+    fonteMaxMinData.style.fontSize = tamanhoAtualData + "%"
+    fonteMaxMinT1.style.fontSize = tamanhoAtualT1 + "%"
+    fonteMaxMinT2.style.fontSize = tamanhoAtualT2 + "%"
+    fonteMaxMinT3.style.fontSize = tamanhoAtualT3 + "%"
+    fonteMaxMinT4.style.fontSize = tamanhoAtualT4 + "%"
+    fonteMaxMinT5.style.fontSize = tamanhoAtualT5 + "%"
+    fonteMaxMinT6.style.fontSize = tamanhoAtualT6 + "%"
+    fonteMaxMinT7.style.fontSize = tamanhoAtualT7 + "%"
+    fonteMaxMinT8.style.fontSize = tamanhoAtualT8 + "%"
+    fonteMaxMinT9.style.fontSize = tamanhoAtualT9 + "%"
+    fonteMaxMinT10.style.fontSize = tamanhoAtualT10 + "%"
+    fonteCard.forEach((el) => el.style.fontSize = tamanhoAtualCard + "%")
     
-        fonteMaxMin.forEach((el) => {el.style.fontSize = tamanhoAtual + "%"})
-        titulo1.style.fontSize = tamanhoAtualT1 + "%"
-        titulo2.style.fontSize = tamanhoAtualT2 + "%"
-        titulo3.style.fontSize = tamanhoAtualT3 + "%"
-       
-    }
+
 })
 
 
 btnMinFonte.addEventListener('click', () => {
-    if(tamanhoAtual > 50 || tamanhoAtualT1 > 50 || tamanhoAtualT2 > 50 || tamanhoAtualT3 < 500) {
-        tamanhoAtual -= 5
-        tamanhoAtualT1 -= 40
-        tamanhoAtualT2 -= 40
-        tamanhoAtualT3 -= 20
-       
-
-        fonteMaxMin.forEach((el) => {el.style.fontSize = tamanhoAtual + "%"})
-        titulo1.style.fontSize = tamanhoAtualT1 + "%"
-        titulo2.style.fontSize = tamanhoAtualT2 + "%"
-        titulo3.style.fontSize = tamanhoAtualT3 + "%"
-    }
+    if(tamanhoAtualNav >= 50 ) tamanhoAtualNav -= 10
+    if(tamanhoAtualData >= 50) tamanhoAtualData -= 10
+    if(tamanhoAtualT1 >= 100) tamanhoAtualT1 -= 30
+    if(tamanhoAtualT2 >= 100) tamanhoAtualT2 -= 30
+    if(tamanhoAtualT3 >= 100) tamanhoAtualT3 -= 20
+    if(tamanhoAtualT4 >= 50) tamanhoAtualT4 -= 10
+    if(tamanhoAtualT5 >= 50) tamanhoAtualT5 -= 10
+    if(tamanhoAtualT6 >= 50) tamanhoAtualT6 -= 20
+    if(tamanhoAtualT7 >= 50) tamanhoAtualT7 -= 10
+    if(tamanhoAtualT8 >= 50) tamanhoAtualT8 -= 10
+    if(tamanhoAtualT9 >= 50) tamanhoAtualT9 -= 10
+    if(tamanhoAtualT10 >= 50) tamanhoAtualT10 -= 20
+    if(tamanhoAtualCard >= 50) tamanhoAtualCard -= 10
+  
+   
+    fonteMaxMinNav.style.fontSize = tamanhoAtualNav + "%"
+    fonteMaxMinData.style.fontSize = tamanhoAtualData + "%"
+    fonteMaxMinT1.style.fontSize = tamanhoAtualT1 + "%"
+    fonteMaxMinT2.style.fontSize = tamanhoAtualT2 + "%"
+    fonteMaxMinT3.style.fontSize = tamanhoAtualT3 + "%"
+    fonteMaxMinT4.style.fontSize = tamanhoAtualT4 + "%"
+    fonteMaxMinT5.style.fontSize = tamanhoAtualT5 + "%"
+    fonteMaxMinT6.style.fontSize = tamanhoAtualT6 + "%"
+    fonteMaxMinT7.style.fontSize = tamanhoAtualT7 + "%"
+    fonteMaxMinT8.style.fontSize = tamanhoAtualT8 + "%"
+    fonteMaxMinT9.style.fontSize = tamanhoAtualT9 + "%"
+    fonteMaxMinT10.style.fontSize = tamanhoAtualT10 + "%"
+    fonteCard.style.fontSize = tamanhoAtualCard + "%"
+    
+    
 })
 
 
